706a13c0d1cd6afb24b33ef77de75cba5789f27b
Daniel Steele <danielsteele@hotmail.co.uk>
2017-02-18 16:47:33 +0000

    Specifying unused block args

--- retro_ken/hooks/reaction.rb
+++ retro_ken/hooks/reaction.rb
@@ -1,6 +1,6 @@
 module RetroKen
   class Lel < SlackRubyBot::Server
-    on 'reaction_added' do |client, data, match|
+    on 'reaction_added' do |_client, data, _match|
       reaction = Reaction.new(ts: data.ts, reaction: data.reaction)
       Message.find_by_ts(data.item.ts).reactions << reaction
     end
