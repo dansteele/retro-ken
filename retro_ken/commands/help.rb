30239b9472f501c8a3955645a0295e4a6aecdce6
Daniel Steele <danielsteele@hotmail.co.uk>
2017-02-24 17:24:18 +0000

    Providing correct URL

--- retro_ken/commands/help.rb
+++ retro_ken/commands/help.rb
@@ -2,7 +2,7 @@ module RetroKen
   module Commands
     class Help < SlackRubyBot::Commands::Base
       command 'help' do |client, data, _match|
-        client.say(channel: data.channel, text: 'See https://github.com/dblock/retro_ken, please.', gif: 'help')
+        client.say(channel: data.channel, text: 'See https://github.com/dansteele/retro-ken, please.', gif: 'help')
       end
     end
   end
