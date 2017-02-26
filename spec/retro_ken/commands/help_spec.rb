9e9e0e83879d7f0bcb92fe6cb1a51c5ab4a20996
Daniel Steele <danielsteele@hotmail.co.uk>
2017-02-26 10:57:17 +0000

    Correcting URL

--- spec/retro_ken/commands/help_spec.rb
+++ spec/retro_ken/commands/help_spec.rb
@@ -5,6 +5,6 @@ describe RetroKen::Commands::Help do
     RetroKen::Bot.instance
   end
   it 'help' do
-    expect(message: 'retroken help').to respond_with_slack_message('See https://github.com/dblock/retro_ken, please.')
+    expect(message: 'retroken help').to respond_with_slack_message('See https://github.com/dansteele/retro-ken, please.')
   end
 end
