d5db6e446e9f767ad6aa44b0db8b3d0ac81afcb1
Daniel Steele <danielsteele@hotmail.co.uk>
2017-03-31 10:02:07 +0100

    Specifying host

--- db/init.rb
+++ db/init.rb
@@ -2,5 +2,6 @@ raise StandardError.new('Specify RACK_ENV') unless ENV['RACK_ENV']
 ActiveRecord::Base.establish_connection(
   adapter:  'postgresql',
   password: ENV['RETRO_KEN_DATABASE_PASSWORD'],
-  database: "retro_ken_#{ENV['RACK_ENV']}"
+  database: "retro_ken_#{ENV['RACK_ENV']}",
+  host: ENV['DATABASE_URL']
 )
